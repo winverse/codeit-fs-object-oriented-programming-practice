e72099757689f28854d20b339bab17ae785ed534
winverse <public.winverse@gmail.com>
2026-09-04 17:48:54 +0900

    fix(topic-31): practice TODO 계약을 명확히 한다

--- src/02-shared-method/problem.js
+++ src/02-shared-method/problem.js
@@ -2,6 +2,7 @@
 class User {
   constructor(email) {
     this.email = email;
+    // TODO: 인스턴스마다 만드는 buy를 class 본문의 메서드로 옮기세요.
     this.buy = function (item) {
       return `${this.email} buys ${item.name}`;
     };
